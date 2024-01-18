43ce9cf25d3d7bb436f6a874c6dd600bf94bb8fe
braussjoss <brauliojossafat@hotmail.com>
2024-01-18 17:11:06 -0600

    Switch to using tfe_outputs

--- resources.tf
+++ resources.tf
@@ -26,10 +26,10 @@ locals {
 ##################################################################################
 
 resource "aws_instance" "main" {
-  count         = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
+  count         = length(data.tfe_outputs.networking.nonsensitive_values.public_subnet)
   ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
   instance_type = var.instance_type
-  subnet_id     = data.tfe_outputs.networking.nonsensitive_values.public_subnets[count.index]
+  subnet_id     = data.tfe_outputs.networking.nonsensitive_values.public_subnet[count.index]
   vpc_security_group_ids = [
     aws_security_group.webapp_http_inbound_sg.id,
     aws_security_group.webapp_ssh_inbound_sg.id,
@@ -97,7 +97,7 @@ resource "aws_lb" "main" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.webapp_http_inbound_sg.id]
-  subnets            = data.tfe_outputs.networking.nonsensitive_values.public_subnets
+  subnets            = data.tfe_outputs.networking.nonsensitive_values.public_subnet
 
   enable_deletion_protection = false
 
